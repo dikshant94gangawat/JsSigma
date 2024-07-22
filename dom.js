3d6f9d6f802ec1bc384bb3e3a54f2770ea92e82c
dikshant gangawat <dikshantgangawat@dikshants-MacBook-Air.local>
2024-07-22 19:42:56 +0530

    whole js code snippets

---
+++ dom.js
@@ -0,0 +1,15 @@
+//getElementByTagName
+
+// var a = document.getElementsByTagName('li');
+// console.log(a);
+
+
+
+//getElementByClassName
+
+// var a = document.getElementsByClassName('hero');
+// console.log(a);
+
+//queryselector
+
+//getboudingclientrect
